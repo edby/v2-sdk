573c5f53a759e6dca1f8da8c624025d528807566
edby <edby@qq.com>
2021-07-11 03:12:43 +0800

    first comnit

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x28D5e44d91eB0b69320B1fAa0eAB0c0022285EBa";
+export declare const INIT_CODE_HASH = "0x736c9a43d038946f5e89b740ae9de9ad06a09f551dfb6b21753f2b6514d9af2e";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x28D5e44d91eB0b69320B1fAa0eAB0c0022285EBa'
+
+export const INIT_CODE_HASH = '0x736c9a43d038946f5e89b740ae9de9ad06a09f551dfb6b21753f2b6514d9af2e'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
